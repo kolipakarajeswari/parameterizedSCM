def61928bb84a214f80f76f3ce72f8ee2dc3062a
kolipakarajeswari <kolipaka.rajeswari.kr@gmail.com>
2026-08-21 12:19:13 +0530

    sumjava

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum
+  {
+    public static void main(String args[]){
+      string s1=args[0];
+      string s2=args[1];
+      int a=Integer.parseInt(s1);
+       int b=Integer.parseInt(s2);
+      System.out.println("sum="+(a+b));
+    }
+  }
+      
